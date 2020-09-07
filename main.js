4c5f6a07bae7b7fb96385c76e136c162abc263cd
Ryson <rysonshalvis@gmail.com>
2020-09-06 21:58:55 -0600

    add styling to page

--- main.js
+++ main.js
@@ -6,21 +6,23 @@ submitButton.addEventListener('click', addTodo);
 list.addEventListener('click', deleteItem)
 
 function addTodo () {
-    console.log(todo.value)
-    let listItem = document.createElement('li');
-    listItem.innerHTML = todo.value + '<button class="delete">Delete</button><button class="completed">Complete</button>';
-    list.appendChild(listItem);
-    todo.value = '';
+    if (todo.value) {
+        let listItem = document.createElement('li');
+        listItem.classList.add('list-item');
+        listItem.innerHTML = todo.value + '<div class="item-container"><button class="delete"><i class="fas fa-trash"></i></button><button class="completed"><i class="fas fa-check"></i></button></div>';
+        list.appendChild(listItem);
+        todo.value = ''; 
+    }
     
  
 }
     
 function deleteItem(event) {
     if (event.target.classList[0] === 'delete') {
-        event.target.parentElement.remove();
+        event.target.parentElement.parentElement.remove();
     }
     if (event.target.classList[0] === 'completed') {
-        event.target.parentElement.style.textDecoration = 'line-through';
+        event.target.parentElement.parentElement.style.textDecoration = 'line-through';
         
     }
 }
