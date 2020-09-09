445248e70d6641e4c10b944fbef25c8219de2f1f
Ryson <rysonshalvis@gmail.com>
2020-09-09 15:11:40 -0600

    add filter option for to do list

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
+
+//variables
 const todo = document.querySelector('#todo');
 const submitButton = document.querySelector('#submit');
 const list = document.querySelector('.list');
+const filterOption = document.querySelector('#option-filter');
+let items = [];
+
+//event listeners
 document.addEventListener('keyup',addTodo);
 submitButton.addEventListener('click', addTodo);
-list.addEventListener('click', deleteItem)
+list.addEventListener('click', deleteItem);
+filterOption.addEventListener('click', filterItems)
 
+//functions
 function addTodo () {
     if (todo.value) {
         if (event.target.classList[0] === 'submit' || event.keyCode === 13) {
@@ -13,10 +21,9 @@ function addTodo () {
         listItem.innerHTML = todo.value + '<div class="item-container"><button class="delete"><i class="fas fa-trash"></i></button><button class="completed"><i class="fas fa-check"></i></button></div>';
         list.appendChild(listItem);
             todo.value = ''; 
+            items.push(listItem);
         }
     }
-    
- 
 }
     
 function deleteItem(event) {
@@ -30,6 +37,34 @@ function deleteItem(event) {
     }
     if (event.target.classList[0] === 'completed') {
         event.target.parentElement.parentElement.style.textDecoration = 'line-through';
-        event.target.parentElement.parentElement.style.opacity = '.5';  
+        event.target.parentElement.parentElement.style.opacity = '.5';
+        event.target.parentElement.parentElement.classList.add('is-completed') 
     }
 }
+
+function filterItems () {
+    if (filterOption.value === 'all') {
+        items.forEach(function(element) {
+           element.style.display = 'flex';
+        }) 
+    }
+    if (filterOption.value === 'completed') {
+        items.forEach(function(element) {
+            if (element.classList.contains('is-completed')) {
+            element.style.display = 'flex';  
+            } else {
+                element.style.display = 'none';
+            } 
+        })    
+    }
+    if (filterOption.value === 'uncompleted') {
+        items.forEach(function(element) {
+            if (element.classList.contains('is-completed') !== true) {
+            element.style.display = 'flex';  
+            } else {
+                element.style.display = 'none';
+            } 
+        })    
+    }
+}
+
